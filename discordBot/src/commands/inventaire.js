beb82f93c00794dde561ffa896d169e95a1684ca
Louis Thomazeau-Agullo <166228818+Louis-Yuichi@users.noreply.github.com>
2024-12-12 08:56:41 +0100

    Dépot du projet

---
+++ discordBot/src/commands/inventaire.js
@@ -0,0 +1,10 @@
+module.exports =
+{
+	name: 'inventaire',
+	description: 'Regarde ton inventaire',
+
+	async execute(interaction) 
+	{
+		await interaction.reply(`en dev...`);
+	}
+};
---
+++ discordBot/src/commands/mine.js
@@ -0,0 +1,10 @@
+module.exports =
+{
+	name: 'mine',
+	description: 'Mine des ressources',
+
+	async execute(interaction)
+	{
+		await interaction.reply(`en dev...`);
+	}
+};
